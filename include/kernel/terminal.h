83f7133ba6db45d2849af2661f3220002ffe5726
user <qwqw@posteo.de>
2018-03-16 18:51:33 +0100

    update

--- include/kernel/terminal.h
+++ include/kernel/terminal.h
@@ -1,25 +1,41 @@
+#include <stdint.h>
 #include <kernel/peripheral.h>
 
 #ifndef TERMINAL_H
 #define TERMINAL_H
+       
+#define TERMINAL_SIZE 128*128
+#define TERMINAL_DEFAULT_CHAR 0x7f
+#define TERMINAL_DEFAULT_COLOUR 0x00
+#define TERMINAL_VIEW_SIZE (SCREEN_WIDTH / 8)*(SCREEN_HEIGHT / 16)
+
+/*uint32_t size = 128*128;
+char character = 0x7f;
+char colour = 0x0;*/
+
+/*uint32_t size = (SCREEN_WIDTH / 8)*(SCREEN_HEIGHT / 16);
+char character = 0x7f;
+char colour = 0x0;*/
 
 struct terminal_buffer {
-        uint32_t size = 128*128;
-        char character = 0x7f;
-        char colour = 0x0;
+        uint32_t size;
+        char character;
+        char colour;
 };
 
 struct terminal_screen {
-        uint32_t size = (SCREEN_WIDTH / 8)*(SCREEN_HEIGHT / 16);
-        char character = 0x7f;
-        char colour = 0x0;
-}
+        uint32_t size;
+        char character;
+        char colour;
+};
 
-struct terminal_buffer terminal_start;
-struct terminal_buffer terminal_stop;
-struct terminal_buffer terminal_view;
-uint32_t terminal_colour;
+struct terminal_buffer *terminal_start;
+struct terminal_buffer *terminal_stop;
+struct terminal_buffer *terminal_view;
 
+uint32_t terminal_curr_colour;
+void terminal_init(void);
 void terminal_colour(char colour);
+void terminal_display(void);
 
 #endif
--- src/kernel/terminal.c
+++ src/kernel/terminal.c
@@ -22,13 +22,52 @@ uint32_t colours[16] = {
         0xffffff //white
 };
 
+char *colour_names[] = {
+        "black",
+        "blue", 
+        "green",
+        "cyan",
+        "red",
+        "magenta",
+        "brown",
+        "light grey",
+        "grey",
+        "light blue",
+        "light green",
+        "light cyan",
+        "light red",
+        "light magenta",
+        "yellow",
+        "white"
+};
+
+void terminal_init(void)
+{
+        terminal_start->size = TERMINAL_SIZE;
+        terminal_start->character = TERMINAL_DEFAULT_CHAR;
+        terminal_start->colour = TERMINAL_DEFAULT_COLOUR;
+
+        terminal_stop->size = TERMINAL_SIZE;
+        terminal_stop->character = TERMINAL_DEFAULT_CHAR;
+        terminal_stop->colour = TERMINAL_DEFAULT_COLOUR;
+
+        terminal_view->size = TERMINAL_SIZE;
+        terminal_view->character = TERMINAL_DEFAULT_CHAR;
+        terminal_view->colour = TERMINAL_DEFAULT_COLOUR;
+
+        terminal_curr_colour = TERMINAL_DEFAULT_COLOUR;
+}
+
 void terminal_colour(char colour) 
 {
+        if(colour > 15)
+                return; // invalid colour. Do nothing
+
         draw_set_forecolour(colour); //set 4bit colour to 16bit equivalent
 }
 
 
-void terminal_display()
+void terminal_display(void)
 {
         int x, y;
         int ROWS = (SCREEN_HEIGHT / 16);
@@ -36,7 +75,7 @@ void terminal_display()
 
         for(y = 0; y < ROWS; y++) {
                 for(x = 0; x < COLS; x++) {
-                        
                 }
         }
 }
+
